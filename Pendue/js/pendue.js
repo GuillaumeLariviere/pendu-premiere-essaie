649281bc8f90f85f58a94c68de94138aa0084ec4
GuillaumeLariviere <87020462+GuillaumeLariviere@users.noreply.github.com>
2021-07-21 20:10:04 +0200

    Add files via upload

--- Pendue/js/pendue.js
+++ Pendue/js/pendue.js
@@ -1,6 +1,52 @@
 let d = document;
-let lettre = d.querySelectorAll('.rond')
-let Tabsmots=["bonjour","salut"]
+let lettre = d.querySelectorAll('.rond');
+let mots=["bonjour","salut"];
+let motATrouve=RandomMots()
+let nbLettre=motATrouve.length
 
+
+function startGame(){
+    start=d.querySelector('#Start');
+    start.addEventListener('clic',startGame());
+    lettre.forEach(function(listeLettre){
+        listeLettre.classList.remove('.d-none');
+        listeLettre.addEventListener('clic',btn);
+            function btn(){
+                listeLettre.classList.add('.d-none')
+
+    }
+},)
+
+
+
+
+
+   
+   
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+startGame()
+
+function RandomMots(){
+    let random = Math.floor(Math.random() * mots.length);
+    let mot = mots[random];
+    mot = mot.replace("â","a").replace("à","a").replace("ï","i").replace("î","i").replace("ô","o").replace("ö","o").replace("é","e").replace("è","e").replace("ê","e").replace("ç","c");
+    mot = mot.toLowerCase();       
+    return mot;
+}
 
 let pb;
