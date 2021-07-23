c2da7f7215f0011b909d96c46b7a9b54bbafc3e9
GuillaumeLariviere <guillaume.lariviere59@gmail.com>
2021-07-23 10:46:29 +0200

    suppression des élément inutile et déco a faire

--- Pendue/js/pendue.js
+++ Pendue/js/pendue.js
@@ -1,14 +1,10 @@
 let d = document;
-let lettre = d.querySelectorAll('.rond');
+let lettres = d.querySelectorAll('.rond');
 let Spe =[" ", "'","-"] ;
 let mots=words_list;
-let listeLettre;
-let ltr;
-// let nbLettre;
-
-
 let image =1;
 let motATrouve;
+
 function startGame(){
      motATrouve=RandomMots();
     console.log(motATrouve);
@@ -25,35 +21,28 @@ function startGame(){
         }
         if(Spe[2].includes(motATrouve[i])){
             d.querySelectorAll('.lettre')[i].innerText=Spe[2]
-
         }
         else{
-
         }
     }
-
     image =1;
 
-
-    lettre.forEach(function(listeLettre){
+    lettres.forEach(function(listeLettre){
         listeLettre.classList.remove('transparance');
         listeLettre.addEventListener('click',btn);
         listeLettre.removeAttribute('disabled')  ;
     });
-    
 }
 
 function btn(event){
-    event.target.classList.remove('.rond');
+    event.target.classList.remove('rond');
     event.target.classList.add('transparance');
     let ltr= event.target.value;
-    
     event.target.removeEventListener('click',btn);
     event.target.setAttribute('disabled', true);
     let bonneReponse=false;
     
     for(i=0 ;i<motATrouve.length; i++){
-            
         if(ltr==motATrouve[i]){
             d.querySelectorAll('.lettre')[i].innerText=ltr;
          bonneReponse=true;
@@ -64,7 +53,7 @@ function btn(event){
             console.log(image)
         }
         if(image == 8){
-            lettre.forEach(function(boutonEnd){
+            lettres.forEach(function(boutonEnd){
                 boutonEnd.setAttribute('disabled',true)});
                 alert("Game Over")
         }
@@ -72,7 +61,6 @@ function btn(event){
             alert("victoire")
         }
 }
-
 
 function RandomMots(){
     let random = Math.floor(Math.random() * mots.length);
@@ -82,16 +70,6 @@ function RandomMots(){
     mot = mot.toLowerCase();       
     return mot;
 }
-
-// function btn(event){
-//     event.target.classList.remove('.rond')
-//     event.target.classList.add('transparance')
-//     let ltr= event.target.value;
-//     d.querySelector('#MtAtrv').innerHTML=ltr;
-//     event.target.removeEventListener('click',btn)
-//     event.target.setAttribute('disabled', true)
- 
-// }
 
 startGame()
 
